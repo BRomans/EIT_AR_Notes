2a013596f257219a8d614582d49576a6fddda72f
bromans <michele.romani.gzl0@gmail.com>
2020-01-27 00:30:12 +0100

    :bug: fixed bug in deserialising the server response

--- AR_Notes/Assets/Scripts/APIManager.cs
+++ AR_Notes/Assets/Scripts/APIManager.cs
@@ -10,47 +10,53 @@ public class APIManager : MonoBehaviour
 
     private string server = "localhost";
     private string port = "8080";
-    public TasksList currentTasks;
-    public UsersList currentUsers;
+    public Task[] currentTasks;
+    public User[]  currentUsers;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentUsers = GetUsers();
-        currentTasks = GetTasks();
-
-        Debug.Log("Tasks: " + currentTasks);
-        Debug.Log("Users: " + currentUsers);
-        
+        updateCurrentState();       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        updateCurrentState();
     }
 
-    private TasksList GetTasks()
+    void updateCurrentState() {
+        currentUsers = GetUsers();
+        currentTasks = GetTasks(); 
+    }
+
+    private Task[] GetTasks()
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://{0}:{1}/tasks/all",  server, port));
         //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", 
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
-        string jsonResponse = reader.ReadToEnd();
+        string jsonResponse = fixJson(reader.ReadToEnd());
         Debug.Log("Task Response" + jsonResponse);
-        TasksList tasks = JsonUtility.FromJson<TasksList>(jsonResponse);
+        Task[] tasks = JsonHelper.FromJson<Task>(jsonResponse);
         return tasks;
     }
 
-    private UsersList GetUsers()
+    private User[] GetUsers()
     {
         HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://{0}:{1}/users/all",  server, port));
         //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(String.Format("http://api.openweathermap.org/data/2.5/weather?id={0}&APPID={1}", 
         HttpWebResponse response = (HttpWebResponse)request.GetResponse();
         StreamReader reader = new StreamReader(response.GetResponseStream());
-        string jsonResponse = reader.ReadToEnd();
+        string jsonResponse = fixJson(reader.ReadToEnd());
         Debug.Log("User Response" + jsonResponse);
-        UsersList users = JsonUtility.FromJson<UsersList>(jsonResponse);
+        User[] users = JsonHelper.FromJson<User>(jsonResponse);
         return users;
+    }
+
+    string fixJson(string value)
+    {
+        value = "{\"Items\":" + value + "}";
+        return value;
     }
 }
