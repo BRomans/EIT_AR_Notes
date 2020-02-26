a7201f2ace46019a201df2fcd19f368080b83ceb
bromans <michele.romani.gzl0@gmail.com>
2020-02-27 00:53:43 +0100

    :sparkles: add collision detection between tasks

---
+++ AR_Notes/Assets/Scripts/CopyTaskContent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyTaskContent : MonoBehaviour
+{
+
+
+    /// <summary>
+    /// Copy the content of a Task on an empty colliding task
+    /// </summary>
+    /// <param name="other">Collider of the section</param>
+    void OnTriggerEnter(Collider other)
+    {  
+        
+        if (other.gameObject.tag == "Task")
+        {
+            Debug.Log("It's a task");
+        }
+    }
+}
