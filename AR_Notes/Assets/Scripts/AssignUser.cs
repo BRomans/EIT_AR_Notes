348678a7027948c7e979ce19fc8ed9e603786425
Rusna Tan <rusna@client-172-18-82-80.clients.u-psud.fr>
2020-02-27 15:41:30 +0100

    :sparkles: implemented user assignment with markers

---
+++ AR_Notes/Assets/Scripts/AssignUser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignUser : MonoBehaviour
+{
+    public Task task;
+    private APIManager apiManager; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {  
+        other.GetComponent<TaskFieldsManager>().ClaimTask(1);
+    }
+}
